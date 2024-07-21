5d64be85257168b0d202388d4b050f381ddaff20
munrojm <jmunro@lbl.gov>
2024-07-20 21:12:57 -0700

    Switch error encoding back to str

--- src/atomicds/core/client.py
+++ src/atomicds/core/client.py
@@ -72,7 +72,7 @@ class BaseClient:
                 return None
 
             raise ClientError(
-                f"Problem retrieving data from {sub_url} with parameters {params}. HTTP Error {response.status_code}: {response.json().get('detail', 'Unknown error')}"
+                f"Problem retrieving data from {sub_url} with parameters {params}. HTTP Error {response.status_code}: {response.text}"
             )
         if len(response.content) == 0:
             return None
